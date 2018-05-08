e628273f2bdea284ce52631a6982f94de9dc243f
Rostom35 <romaindwan@gmail.com>
2018-05-08 17:06:43 +0200

    t2

--- src/main/java/fr/test/continuous/delivery/web/rest/AccountResource.java
+++ src/main/java/fr/test/continuous/delivery/web/rest/AccountResource.java
@@ -76,7 +76,7 @@ public class AccountResource {
     public void activateAccount(@RequestParam(value = "key") String key) {
         Optional<User> user = userService.activateRegistration(key);
         if (!user.isPresent()) {
-            throw new InternalServerErrorException("No user was oops found for this reset key");
+            throw new InternalServerErrorException("No user was oops! found for this reset key");
         }
     }
 
